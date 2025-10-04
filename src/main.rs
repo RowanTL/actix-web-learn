532c43eab957eb6aa1db19b1981873a7fa0648ee
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-10-04 15:17:25 -0500

    testing structs out

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 use actix_web::{App, HttpServer, web};
 use std::sync::Mutex;
 
-struct AppStateWithCounter {
-    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+#[derive(Clone)]
+struct ArbitraryBullShit {
+    bullshit: usize,
 }
 
-async fn index(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    *counter += 1; // <- access counter inside MutexGuard
-
-    format!("Request number: {counter}") // <- response with count
+async fn index(data: web::Data<ArbitraryBullShit>) -> String {
+    format!("Request number: {}\n", data.bullshit) // <- response with count
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Note: web::Data created _outside_ HttpServer::new closure
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
+    let bs = web::Data::new(ArbitraryBullShit { bullshit: 1 });
 
     HttpServer::new(move || {
         // move counter into the closure
         App::new()
-            .app_data(counter.clone()) // <- register the created data
+            .app_data(bs.clone()) // <- register the created data
             .route("/", web::get().to(index))
     })
     .bind(("127.0.0.1", 8080))?
