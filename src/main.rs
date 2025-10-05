1d5e66792040c758127d99e3729158a274d642fb
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-10-04 22:19:06 -0500

    Returning custom errors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use actix_web::{
-    App, Either, Error, HttpRequest, HttpResponse, HttpServer, Responder, body::BoxBody, get,
-    http::header::ContentType, web,
+    App, Either, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result, body::BoxBody,
+    error, get, http::header::ContentType, web,
 };
+use derive_more::derive::{Display, Error};
 use futures::{future::ok, stream::once};
 use serde::Serialize;
 
 type RegisterResult = Either<HttpResponse, Result<&'static str, Error>>;
+
+#[derive(Debug, Display, Error)]
+#[display("my error: {name}")]
+struct MyError {
+    name: &'static str,
+}
 
 #[derive(Serialize)]
 struct MyObj {
@@ -43,6 +50,14 @@ async fn either_index(path: web::Path<bool>) -> RegisterResult {
     }
 }
 
+// Use default implementation for `error_response()` method
+impl error::ResponseError for MyError {}
+
+#[get("/error")]
+async fn error_index() -> Result<&'static str, MyError> {
+    Err(MyError { name: "test" })
+}
+
 #[get("/stream")]
 async fn stream() -> HttpResponse {
     let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
@@ -59,6 +74,7 @@ async fn main() -> std::io::Result<()> {
             .service(index)
             .service(stream)
             .service(either_index)
+            .service(error_index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
