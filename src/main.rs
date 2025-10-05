34406ef2f4040c9ab1eab6642b375950bdfe80df
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-10-04 21:55:04 -0500

    They borrowed Haskell's either lol

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use actix_web::{
-    App, Error, HttpRequest, HttpResponse, HttpServer, Responder, body::BoxBody, get,
+    App, Either, Error, HttpRequest, HttpResponse, HttpServer, Responder, body::BoxBody, get,
     http::header::ContentType, web,
 };
 use futures::{future::ok, stream::once};
 use serde::Serialize;
+
+type RegisterResult = Either<HttpResponse, Result<&'static str, Error>>;
 
 #[derive(Serialize)]
 struct MyObj {
@@ -29,6 +31,18 @@ async fn index() -> impl Responder {
     MyObj { name: "user" }
 }
 
+#[get("/either/{boolean}")]
+async fn either_index(path: web::Path<bool>) -> RegisterResult {
+    let boolean: bool = path.into_inner();
+    if !boolean {
+        // choose Left variant
+        Either::Left(HttpResponse::BadRequest().body("Bad data"))
+    } else {
+        // choose Right variant
+        Either::Right(Ok("Hello!"))
+    }
+}
+
 #[get("/stream")]
 async fn stream() -> HttpResponse {
     let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
@@ -40,8 +54,13 @@ async fn stream() -> HttpResponse {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(stream))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(stream)
+            .service(either_index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
