28136e648e22e33203d6ca340de59292c68f258a
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-10-04 21:40:23 -0500

    serde for json response and streaming bytes for async return

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,47 @@
-use actix_web::{App, HttpResponse, HttpServer, web};
+use actix_web::{
+    App, Error, HttpRequest, HttpResponse, HttpServer, Responder, body::BoxBody, get,
+    http::header::ContentType, web,
+};
+use futures::{future::ok, stream::once};
+use serde::Serialize;
 
-// this function could be located in a different module
-fn scoped_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/test")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+#[derive(Serialize)]
+struct MyObj {
+    name: &'static str,
 }
 
-// this function could be located in a different module
-fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/app")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+// Responder
+impl Responder for MyObj {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        // Create response and set content type
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    MyObj { name: "user" }
+}
+
+#[get("/stream")]
+async fn stream() -> HttpResponse {
+    let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .streaming(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .configure(config)
-            .service(web::scope("/api").configure(scoped_config))
-            .route(
-                "/",
-                web::get().to(|| async { HttpResponse::Ok().body("/") }),
-            )
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index).service(stream))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
